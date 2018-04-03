5cdbefbbde2e27d751f6debafc83be9a95559fc4
Nikolas Corrêa <nmcorrea@inf.ufsm.br>
2018-04-03 02:43:33 -0300

    add question 4, 5 and 6

--- t2/t2parte4.hs
+++ t2/t2parte4.hs
@@ -30,8 +30,8 @@ chi2 os es = sum [((o-e)^2)/e | (o,e) <- zip os es]
 --crack :: String -> String
 --crack cs = encodeStr cs (-factor)
 --           where factor = head (positions (minimum chitab) chitab)
---                 chitab = [ chi2 (rotate n table' ) table | n <- [0..25] ]
---table' = freqs cs
+--                            chitab = [ chi2 (rotate n table' ) table | n <- [0..25] ] 
+-- table' = freqs cs
 
 -- 1) Usando encodeChar e decodeChar, defina uma função shiftChar :: Char -> Int -> Char. Essa função, ao receber um caracter de 'a' a 'z', aplicará um deslocamento de 'n' unidades sobre ele, produzindo outro caracter no intervalo ['a'..'z']
 shiftChar :: Char -> Int -> Char
@@ -44,3 +44,15 @@ encodeStr str x = map (\y -> shiftChar y x) str
 -- 3) Defina uma função countValids :: String -> Int, que receba uma string e retorne a quantidade de seus caracteres contidos no intervalo ['a'..'z']. 
 countValids :: String -> Int
 countValids str = length (filter (\x -> elem x ['a'..'z']) str)
+
+-- 4) Defina uma função countChar :: Char -> String -> Int, que retorne a quantidade de um dado caracter em uma string.
+countChar :: Char -> String -> Int
+countChar x y = length (filter (\z -> x == z) y)
+
+-- 5) Usando countValids, countChar e percent, defina uma função freqs :: String -> [Float] que retorne as frequências dos caracteres ['a'..'z'] numa dada string. Use list comprehension. A frequência de um caracter é dada pelo percentual deste caracter entre os caracteres válidos da string.
+freqs :: String -> [Float]
+freqs str = [percent (countValids str) (countChar x str) | x <- ['a'..'z']]
+
+-- 6) Defina uma função positions :: Float -> [Float] -> [Int], que retorne uma lista de posições de um dado número em uma lista. Considere que as posições comecem em zero. Use a função zip como auxiliar no seu código.
+positions :: Float -> [Float] -> [Int]
+positions x y = [snd z | z <- (zip y [0..length y]), fst z == x]
