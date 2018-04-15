ab749736824c4a293d3869ea872159470c55e19d
Nikolas Corrêa <nmcorrea@inf.ufsm.br>
2018-04-15 17:45:55 -0300

    add question 1

--- t3/t3.pl
+++ t3/t3.pl
@@ -23,3 +23,5 @@ repete(N, C, L) :-
 % ?- repete(3,a,[a|T]).
 % R: T = [a, a].
 
+% 1. Defina um predicado zeroInit(L) que é verdadeiro se L for uma lista que inicia com o número 0 (zero).
+zeroInit(L) :- L = [H|_], H =:= 0.
