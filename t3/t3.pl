e129b35eb94bd16c59dc38aa405c1b287da87db9
Nikolas Corrêa <nmcorrea@inf.ufsm.br>
2018-04-16 01:53:27 -0300

    add question 4,5,6

--- t3/t3.pl
+++ t3/t3.pl
@@ -31,3 +31,22 @@ has5(L) :- L = [_,_,_,_,_].
 
 % 3. Defina um predicado hasN(L,N) que é verdadeiro se L for uma lista de N elementos.
 hasN(L,N) :- length(L,A), A=:=N.
+
+% 4. Defina um predicado potN0(N,L), de forma que L seja uma lista de potências de 2, com expoentes de N a 0.
+potN0(0,[1]).
+potN0(N,L) :- N>0, N1 is N-1, potN0(N1,L1), M is 2^N, insereInicio(M,L1,L).
+
+% Insere um elemento no início de uma lista
+insereInicio(H, T, [H|T]):- !.
+
+% 5. Defina um predicado zipmult(L1,L2,L3), de forma que cada elemento da lista L3 seja o produto dos elementos de L1 e L2 na mesma posição do elemento de L3. 
+zipmult([],[],[]).
+zipmult(L1,L2,L3) :- [A|B] = L1, [C|D] = L2, zipmult(B,D,Y), X is A*C, insereInicio(X,Y,L3). 
+
+% 6. Defina um predicado potencias(N,L), de forma que L seja uma lista com as N primeiras potências de 2, sendo a primeira 2^0 e assim por diante.
+potencias(0,[1]).
+potencias(N,L) :- inverte(L,L1), potN0(N-1,L1).
+
+% Inverte uma Lista
+inverte([], []).
+inverte([H|T], L) :- inverte(T, X), append(X, [H], L).
