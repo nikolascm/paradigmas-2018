6c8f5b45425c6624edd8db1c58299344df87bc15
Nikolas Corrêa <nmcorrea@inf.ufsm.br>
2018-04-16 23:28:13 -0300

    add question 10

--- t3/t3.pl
+++ t3/t3.pl
@@ -52,3 +52,22 @@ positivos(L1,L2) :-
 	L2 = T1.	
 
 % 8. Considere que L1 e L2 sejam permutações de uma lista de elementos distintos, sem repetições. Sabendo disso, defina um predicado mesmaPosicao(A,L1,L2) para verificar se um elemento A está na mesma posição nas listas L1 e L2. 
+mesmaPosicao(A,[A|_],[A|_]).
+mesmaPosicao(A,L1,L2) :-
+	L1 = [_|T1],
+	L2 = [_|T2],
+	mesmaPosicao(A,T1,T2).
+
+% 9. Dada uma lista de N alunos, deseja-se escolher NP alunos (NP < N) para formar uma comissão. Para isso, defina um predicado comissao(NP,LP,C), que permita gerar as possíveis combinações C com NP elementos da lista LP. 
+
+
+% 10. Tem-se N azulejos 10cm x 10cm e, com eles, deve-se montar um conjunto de quadrados de modo a utilizar todos os azulejos dados, sem sobrepô-los. Inicialmente, deve-se montar o maior quadrado possível; então, com os azulejos que sobraram, deve-se montar o maior quadrado possível, e assim sucessivamente. Por exemplo, se forem dados 31 azulejos, o conjunto montado terá 4 quadrados. Para resolver este problema, você deverá definir um predicado azulejos(NA, NQ), de forma que NQ seja o número de quadrados que se deve montar com NA azulejos. 
+azulejos(0,0).
+azulejos(NA,NQ) :-
+	NA>0,
+	R is sqrt(NA),
+	MaxInt is floor(R),
+	MaxQuad is MaxInt^2,
+	Resto is NA-MaxQuad,
+	azulejos(Resto,Cont),
+	NQ is Cont+1.
